bbec339366ab8ea786ca97187ffc14139ce7e1df
Ah ! <i@forgiveher.cn>
2018-07-22 23:07:45 +0800

    Update

---
+++ JsonTreeView/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace JsonTreeview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_json = new System.Windows.Forms.TextBox();
+            this.treeView_json = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_json
+            // 
+            this.textBox_json.Location = new System.Drawing.Point(13, 13);
+            this.textBox_json.Multiline = true;
+            this.textBox_json.Name = "textBox_json";
+            this.textBox_json.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_json.Size = new System.Drawing.Size(775, 169);
+            this.textBox_json.TabIndex = 0;
+            this.textBox_json.Text = "{\r\n\t\"animals\":{\r\n\t\"dog\":[\r\n\t\t{\"name\":\"Rufus\",\"breed\":\"labrador\",\"count\":1,\"twoFee" +
+    "t\":false},\r\n\t\t{\"name\":\"Marty\",\"breed\":\"whippet\",\"count\":1,\"twoFeet\":false}\r\n\t],\r" +
+    "\n\t\"cat\":{\"name\":\"Matilda\"}\r\n}\r\n}";
+            // 
+            // treeView_json
+            // 
+            this.treeView_json.Location = new System.Drawing.Point(13, 234);
+            this.treeView_json.Name = "treeView_json";
+            this.treeView_json.Size = new System.Drawing.Size(775, 204);
+            this.treeView_json.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Parse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView_json);
+            this.Controls.Add(this.textBox_json);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_json;
+        private System.Windows.Forms.TreeView treeView_json;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ JsonTreeView/Form1.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JsonTreeview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView_json.Nodes.Clear();
+            TreeNode node = null;
+            JObject jObject = JObject.Parse(textBox_json.Text); //添加异常处理
+            if (jObject.GetType() == typeof(Newtonsoft.Json.Linq.JObject))
+            {
+                node = treeView_json.Nodes.Add("[Object]");
+            }
+            else if (jObject.GetType() == typeof(Newtonsoft.Json.Linq.JArray))
+            {
+                node = treeView_json.Nodes.Add("[Array]");
+            }
+            foreach (var obj in jObject)
+            {
+                showJsonTree(treeView_json, jObject, obj.Key.ToString(), null, node);
+            }
+        }
+
+        private void showJsonTree(TreeView tree, JObject jObject, String key, string title = null, TreeNode node = null)
+        {
+            string text = "";
+            TreeNode index;
+            if (jObject[key].GetType() == typeof(Newtonsoft.Json.Linq.JArray))
+            {
+                text = key + ":[Array]";
+            }
+            else if (jObject[key].GetType() == typeof(Newtonsoft.Json.Linq.JObject))
+            {
+                text = key + ":[Object]";
+            }
+            else if (jObject[key].GetType() == typeof(Newtonsoft.Json.Linq.JValue))
+            {
+                if ((string)jObject[key] == null)
+                {
+                    text = key + ":" + "null";
+                }
+                else { text = key + ":" + (string)jObject[key]; }
+            }
+            if (title != null)
+            {
+                text = title + ":" + text;
+            }
+            index = node.Nodes.Add(text);
+            if (jObject[key].GetType() == typeof(Newtonsoft.Json.Linq.JObject))
+            {
+                JObject @object = JObject.Parse(jObject[key].ToString());
+                foreach (var obj in @object)
+                {
+                    showJsonTree(tree, @object, obj.Key, null, index);
+                }
+            }
+            else if (jObject[key].GetType() == typeof(Newtonsoft.Json.Linq.JArray))
+            {
+                JArray jArray = JArray.Parse(jObject[key].ToString());
+                int inx = -1;
+                foreach (var obj in jArray)
+                {
+                    inx++;
+                    node = index.Nodes.Add("[" + inx + "]");
+                    if (obj.GetType() == typeof(Newtonsoft.Json.Linq.JObject))
+                    {
+                        JObject @object = JObject.Parse(obj.ToString());
+                        foreach (var ob in @object)
+                        {
+                            showJsonTree(tree, @object, ob.Key, null, node);
+                        }
+                    }
+                    else if (obj.GetType() == typeof(Newtonsoft.Json.Linq.JValue))
+                    {
+                        if (obj.ToString() == null)
+                        {
+                            node.Nodes.Add("null");
+                        }
+                        else { node.Nodes.Add(obj.ToString()); }
+                    }
+                }
+            }
+        }
+    }
+}
